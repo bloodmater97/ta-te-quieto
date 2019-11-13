4c6571838d8147a11dfadc19d9ebcca3405bf789
mdemartino26 <mdmyec@gmail.com>
2019-11-13 20:53:15 -0300

    empate

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@ var posicion=0; // valida la posicion de cada array
 var turno = 1; // servira para validar los turnos durante el juego - BULEANO
 var j1 = "X"; //Declaro la variable del jugador 1
 var j2 = "O"; //Declaro la variable del jugador 2
-var contador = 0; // Hago un contador que va a ir alternando los turnos
 var columna = [[],[],[]];
 var k=1;
 var puedeSeguir = true;//Esto va a ver si se puede seguir jugando o no, ya que si hay una combinación ganadora no va a permitir que se siga jugando.
@@ -47,18 +46,18 @@ function genera_tabla() {
 }
 
 // Funcion que alterna los turnos y muestra las X e O
-function turnos (a) {
+function turnos (idCelda) {
   if(puedeSeguir == true){
-    if(document.getElementById(a).innerHTML == ""){
-      positions(a);//Se lo llama fuera de los otros if, porque pregunta si hay un lugar vacío para seguir jugando. Si lo hay va a llamar a la otra función sin importar de quién sea el turno. Ya se tiene el valor con la variable turno y qué se va a poner en el casillero que se haga click.
+    if(document.getElementById(idCelda).innerHTML == ""){
+      positions(idCelda);//Se lo llama fuera de los otros if, porque pregunta si hay un lugar vacío para seguir jugando. Si lo hay va a llamar a la otra función sin importar de quién sea el turno. Ya se tiene el valor con la variable turno y qué se va a poner en el casillero que se haga click.
       if (turno === 1) {
-        document.getElementById(a).innerHTML= j1;
+        document.getElementById(idCelda).innerHTML= j1;
         document.getElementById("datos").innerHTML = "Turno del Jugador 1";
         verificarGanador();
         turno = 0;
       }
       else if (turno === 0) {
-        document.getElementById(a).innerHTML= j2;
+        document.getElementById(idCelda).innerHTML= j2;
         document.getElementById("datos").innerHTML = "Turno del jugador 2";
         verificarGanador();
         turno = 1;
@@ -67,30 +66,36 @@ function turnos (a) {
   }
 }
 
-function positions(a) {//El parametro a tiene los Id de las celdas. Osea que si a es = al Id de la celda que se está clickeando, la "X" o la "O" que se va a poner en ese lugar también la va a tener la matriz. Al tener este dato la matriz después va a poder verificar quién ganó.
-  if(a == "zelda1"){
+function positions(idCelda) {//El parametro a tiene los Id de las celdas. Osea que si a es = al Id de la celda que se está clickeando, la "X" o la "O" que se va a poner en ese lugar también la va a tener la matriz. Al tener este dato la matriz después va a poder verificar quién ganó.
+  if(idCelda == "zelda1"){
     columna[0][0] = turno;
-  } else if (a == "zelda2"){
+  } else if (idCelda == "zelda2"){
     columna[0][1] = turno;
-  } else if (a == "zelda3"){
+  } else if (idCelda == "zelda3"){
     columna[0][2] = turno;
-  } else if (a == "zelda4"){
+  } else if (idCelda == "zelda4"){
     columna[1][0] = turno;
-  } else if (a == "zelda5"){
+  } else if (idCelda == "zelda5"){
     columna[1][1] = turno;
-  } else if (a == "zelda6"){
+  } else if (idCelda == "zelda6"){
     columna[1][2] = turno;
-  } else if (a == "zelda7"){
+  } else if (idCelda == "zelda7"){
     columna[2][0] = turno;
-  } else if (a == "zelda8"){
+  } else if (idCelda == "zelda8"){
     columna[2][1] = turno;
-  } else if (a == "zelda9"){
+  } else if (idCelda == "zelda9"){
     columna[2][2] = turno;
   }
 }
 
 //Funcion que verifica si alguien gano.
 function verificarGanador(){
+  var jugador;
+  if (turno == 1) {
+    jugador = "jugador 1";
+  } else {
+    jugador = "jugador 2";
+  }
 //Cada if verifica todas las posiciones existentes en el juego.
    if (columna[0][0] == turno && columna [0][1] == turno && columna [0][2] == turno){
      
@@ -98,7 +103,8 @@ function verificarGanador(){
      document.getElementById("zelda2").style.color = "red";
      document.getElementById("zelda3").style.color = "red";
      puedeSeguir = false;
-     document.getElementById("datos").innerHTML = "Fin del juego";
+     document.getElementById("datos").innerHTML = "Fin del juego, ganador: " + jugador;
+     mostrarCartel(jugador);
 
     } else if (columna[1][0] === turno && columna[1][1] === turno && columna[1][2] === turno){
       
@@ -106,7 +112,8 @@ function verificarGanador(){
      document.getElementById("zelda5").style.color = "red";
      document.getElementById("zelda6").style.color = "red";
      puedeSeguir = false;
-     document.getElementById("datos").innerHTML = "Fin del juego";
+     document.getElementById("datos").innerHTML = "Fin del juego, ganador: " + jugador;
+     mostrarCartel(jugador);
 
     } else if (columna[2][0] === turno && columna[2][1] === turno && columna[2][2] === turno){
       
@@ -114,7 +121,8 @@ function verificarGanador(){
      document.getElementById("zelda8").style.color = "red";
      document.getElementById("zelda9").style.color = "red";
      puedeSeguir = false;
-     document.getElementById("datos").innerHTML = "Fin del juego";
+     document.getElementById("datos").innerHTML = "Fin del juego, ganador: " + jugador;
+     mostrarCartel(jugador);
 
     } else if (columna[0][0] === turno && columna[1][0] === turno && columna[2][0] === turno){
 
@@ -122,7 +130,8 @@ function verificarGanador(){
      document.getElementById("zelda4").style.color = "red";
      document.getElementById("zelda7").style.color = "red";
      puedeSeguir = false;
-     document.getElementById("datos").innerHTML = "Fin del juego";
+     document.getElementById("datos").innerHTML = "Fin del juego, ganador: " + jugador;
+     mostrarCartel(jugador);
 
     } else if (columna[0][1] === turno && columna[1][1] === turno && columna[2][1] === turno){
 
@@ -130,7 +139,8 @@ function verificarGanador(){
      document.getElementById("zelda5").style.color = "red";
      document.getElementById("zelda8").style.color = "red";
      puedeSeguir = false;
-     document.getElementById("datos").innerHTML = "Fin del juego";
+     document.getElementById("datos").innerHTML = "Fin del juego, ganador: " + jugador;
+     mostrarCartel(jugador);
 
     } else if (columna[0][2] === turno && columna[1][2] === turno && columna[2][2] === turno){
 
@@ -138,7 +148,8 @@ function verificarGanador(){
      document.getElementById("zelda6").style.color = "red";
      document.getElementById("zelda9").style.color = "red";
      puedeSeguir = false;
-     document.getElementById("datos").innerHTML = "Fin del juego";
+     document.getElementById("datos").innerHTML = "Fin del juego, ganador: " + jugador;
+     mostrarCartel(jugador);
 
     } else if (columna[0][0] === turno && columna[1][1] === turno && columna[2][2] === turno){
 
@@ -146,7 +157,8 @@ function verificarGanador(){
      document.getElementById("zelda5").style.color = "red";
      document.getElementById("zelda9").style.color = "red";
      puedeSeguir = false;
-     document.getElementById("datos").innerHTML = "Fin del juego";
+     document.getElementById("datos").innerHTML = "Fin del juego, ganador: " + jugador;
+     mostrarCartel(jugador);
 
     } else if (columna[0][2] === turno && columna[1][1] === turno && columna[2][0] === turno){
 
@@ -154,23 +166,48 @@ function verificarGanador(){
      document.getElementById("zelda5").style.color = "red";
      document.getElementById("zelda7").style.color = "red";
      puedeSeguir = false;
-     document.getElementById("datos").innerHTML = "Fin del juego";
+     document.getElementById("datos").innerHTML = "Fin del juego, ganador: " + jugador;
+     mostrarCartel(jugador);
   }else{
-    recorreArray();
+    esEmpate()
   }
 }
 
-function recorreArray(){
-    for (var i = 1; i <= 9; i++){
-      empate();
+function esEmpate() {
+  for (var i = 1; i <= 9; i++){
+    if(document.getElementById("zelda" + i).innerHTML == "") {
+      puedeSeguir = true;
+      todasOcupadas = false;
+      return;
     }
   }
 
-function empate(){
-  if (puedeSeguir === true && document.getElementById("zelda" + i).innerHTML == "") {
-    todasOcupadas = false;
-  } else{
-    puedeSeguir = false;
-    document.getElementById("datos").innerHTML = "empate";
+  puedeSeguir = false;
+  todasOcupadas = true;
+  document.getElementById("datos").innerHTML = "empate";
+  mostrarCartel();
+}
+
+function mostrarCartel(ganador){
+  document.getElementById("cartel").style.display = "block";
+  document.getElementById("ganador").innerHTML = "Fin del juego, ganador: " + ganador;
+  if (!ganador) {
+    document.getElementById("ganador").innerHTML = "Fin del juego, empate";
+  }
+}
+
+function reiniciar(){
+  document.getElementById("cartel").style.display = "none";
+  columna = [[],[],[]];
+  puedeSeguir = true;
+  todasOcupadas = false;
+  turno = 1;
+  limpiarTabla();
+}
+
+function limpiarTabla(){
+  for ( var i=1; i <= 9; i++){
+    document.getElementById("zelda" + i).innerHTML = "";
+    document.getElementById("zelda" + i).style.color = "white";
   }
 }
